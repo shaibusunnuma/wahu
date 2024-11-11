eb878eb3b6306d026ba3b7ef6c7d31ff877051c9
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2024-11-11 01:10:23 +0000

    setup auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import authRouter from "./routes/auth.route";
+import { getTestToken } from "../firebaseClientTest";
+import errorMiddleware from "./middlewares/error.middleware";
 
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.get("/ping", async (req, res) => {
-  res.send("pong");
-});
+app.get("/token/:uid", async (req, res) => {
+ 
+    const token = await getTestToken();
+    res.send(token);
+  });
+
+app.use("/auth", authRouter);
+app.use(errorMiddleware);
 
 
 app.listen(3000, () => {
