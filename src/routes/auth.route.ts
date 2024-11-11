e1932440fb5aa32f9a406e042a9c51ceb76e2664
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2024-11-11 01:55:07 +0000

    add validators to routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController";
-
+import { validateSchema } from "../middlewares/validators/validateRequest";
+import { authSchema } from "../middlewares/validators/authSchema";
 const authRouter = Router();
 
 authRouter.post("/login", AuthController.login);
-authRouter.post("/register", AuthController.register);
+authRouter.post(
+  "/register",
+  validateSchema(authSchema.register),
+  AuthController.register
+);
 
 export default authRouter;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
 import authMiddleware from "../middlewares/auth.middleware";
-
+import { validateSchema } from "../middlewares/validators/validateRequest";
+import { profileSchema } from "../middlewares/validators/profileSchema";
 
 const userRouter = Router();
 userRouter.use(authMiddleware);
 
 userRouter
   .route("")
-  .post(UserController.createUserProfile)
-  .patch( UserController.updateUserProfile)
+  .post(validateSchema(profileSchema.create), UserController.createUserProfile)
+  .patch(validateSchema(profileSchema.update), UserController.updateUserProfile)
   .get(UserController.getUserProfile)
   .delete(UserController.deleteUserProfile);
 
